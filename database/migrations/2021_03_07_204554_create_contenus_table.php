3dcf4bb80803a6228c056c3e3c1994240785d66a
Cedric Lima <limacedric@hotmail.fr>
2021-03-07 21:49:25 +0100

    Création table article

---
+++ database/migrations/2021_03_07_204554_create_contenus_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contenus', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('image');
+            $table->longText('content');
+            $table->unsignedBigInteger('article_id');
+            $table->foreign('article_id')->references('id')->on('articles');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contenus');
+    }
+}
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,40 +1,24 @@
 <template>
-<div>
+
   <div id="fontHome" class="view jarallax" data-jarallax='{"speed": 0.2}'>
     <div class="mask rgba-white-light d-flex justify-content-center align-items-center">
       <div class="container">
         <div class="row">
-          <div class="col-md-12 white-text text-center">
-            <h1 class="display-3 mb-0 pt-md-5 pt-5 white-text font-weight-bold wow fadeInDown" data-wow-delay="3.3s">NATALIE SMITH</h1>
-            <h5 class="text-uppercase pt-md-5 pt-sm-2 pt-5 pb-md-5 pb-sm-3 pb-5 white-text font-weight-bold">Photographe</h5>
+          <div class="col-md-12 text-white text-center">
+            <h1 class="display-3 mb-0 pt-md-5 pt-5 text-white font-weight-bold wow fadeInDown" data-wow-delay="3.3s">NATALIE SMITH</h1>
+            <h1 class="text-uppercase mt-5 pt-md-5 pt-sm-2 pt-5 pb-md-5 pb-sm-3 pb-5 text-white font-weight-bold">Photographe</h1>
           </div>
         </div>
       </div>
-      
     </div>
-
-
-</div>
+   </div>
 
 <!-- <img :src="'storage/images/home.jpg'">  -->
 
-
-
-</div>
 </template>
 <script>
 
 export default {
-   
-   
+
 }
 </script>
-
-<style> 
-#fontHome{
-   background-image: url('/storage/images/home.jpg'); 
-   background-repeat: no-repeat; 
-   background-size: cover; 
-   background-position: center center;
-}
-</style>
