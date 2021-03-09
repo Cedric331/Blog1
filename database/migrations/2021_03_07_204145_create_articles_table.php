34fb3933177f1d77d228df96b42077b641dd188f
Cedric Lima <limacedric@hotmail.fr>
2021-03-09 14:51:42 +0100

    Suppression de voyager

--- database/migrations/2021_03_07_204145_create_articles_table.php
+++ database/migrations/2021_03_07_204145_create_articles_table.php
@@ -15,7 +15,7 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('title')->unique();
             $table->string('image');
             $table->string('slug');
             $table->boolean('top')->default(false);
