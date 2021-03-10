e260f497f67eede1767570f77abdbc06f23cc904
Cedric Lima <limacedric@hotmail.fr>
2021-03-10 11:20:20 +0100

    Ajout confirmation avant suppression du commentaire

--- resources/js/Pages/ViewArticle.vue
+++ resources/js/Pages/ViewArticle.vue
@@ -41,7 +41,7 @@
                             <p style="white-space: pre-line;">
                                 {{ commentaire.user.name }}: {{ commentaire.commentaire }}
                             </p>
-                           <div v-if="commentEditFormVisible === commentaire.id"
+                            <div v-if="commentEditFormVisible === commentaire.id"
                                 class="form-group shadow-lg p-3 mb-5 bg-light rounded">
                                 <div v-if="errorsEdit" class="alert alert-danger">
                                     <div v-for="err in errorsEdit" :key="err.message">
@@ -62,16 +62,16 @@
                         </div>
 
                         <ul v-if="user && user.id == commentaire.user.id">
-                           <li class="nav-item dropdown" >
-                              <a class="nav-link" data-toggle="dropdown" href="#" role="button"
-                                  aria-haspopup="true" aria-expanded="false"><i class="fas fa-caret-down"></i></a>
-                              <div class="dropdown-menu">
-                                 <a type="button" @click="formEditComment(commentaire)"
-                                          class="dropdown-item rounded">Modifier</a>
-                                 <a type="button" @click="deleteComment(commentaire.id)"
-                                          class="dropdown-item rounded">Supprimer</a>
-                              </div>
-                          </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
+                                    aria-expanded="false"><i class="fas fa-caret-down"></i></a>
+                                <div class="dropdown-menu">
+                                    <a type="button" @click="formEditComment(commentaire)"
+                                        class="dropdown-item rounded">Modifier</a>
+                                    <a type="button" @click="deleteComment(commentaire.id)"
+                                        class="dropdown-item rounded">Supprimer</a>
+                                </div>
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -159,15 +159,36 @@
                 })
             },
             deleteComment(id) {
-                axios.delete('/commentaire/delete/'+id
-                ).then(res => {
-                    this.errorsEdit = null
-                    this.errors = null
-                    this.commentaires = res.data
-                    this.commentEditVisible = String
-                    this.commentEditFormVisible = Number
-                }).catch(err => {
-                })
+               let message = "Voulez-vous supprimer le commentaire ?";
+ 
+               let options = {
+                   html: false, // set to true if your message contains HTML tags. eg: "Delete <b>Foo</b> ?"
+                   loader: true, // set to true if you want the dailog to show a loader after click on "proceed"
+                   reverse: true, // switch the button positions (left to right, and vise versa)
+                   okText: 'Oui',
+                   cancelText: 'Annuler',
+                   animation: 'fade', // Available: "zoom", "bounce", "fade"
+                   type: 'basic', // coming soon: 'soft', 'hard'
+                   verification: 'continue', // for hard confirm, user will be prompted to type this to enable the proceed button
+                   verificationHelp: 'Type "[+:verification]" below to confirm', // Verification help text. [+:verification] will be matched with 'options.verification' (i.e 'Type "continue" below to confirm')
+                   clicksCount: 3, // for soft confirm, user will be asked to click on "proceed" btn 3 times before actually proceeding
+                   backdropClose: true, // set to true to close the dialog when clicking outside of the dialog window, i.e. click landing on the mask
+                   customClass: '' // Custom class to be injected into the parent node for the current dialog instance
+               };
+                this.$dialog
+                    .confirm(message, options)
+                    .then(dialog => {
+                        axios.delete('/commentaire/delete/' + id).then(res => {
+                            this.errorsEdit = null
+                            this.errors = null
+                            this.commentaires = res.data
+                            this.commentEditVisible = String
+                            this.commentEditFormVisible = Number
+                        }).catch(err => {})
+                        setTimeout(() => {
+                            dialog.close();
+                        }, 500);
+                    })
             },
         },
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,10 @@
 import { App, plugin } from '@inertiajs/inertia-vue'
 import Vue from 'vue'
 import { InertiaProgress } from '@inertiajs/progress'
+import VuejsDialog from 'vuejs-dialog';
+import VuejsDialogMixin from 'vuejs-dialog/dist/vuejs-dialog-mixin.min.js';
+import 'vuejs-dialog/dist/vuejs-dialog.min.css';
+
 
 window.Vue = require('vue');
 window.axios = require('axios');
@@ -13,7 +17,7 @@ const app = new Vue({
 
 InertiaProgress.init();
 Vue.use(plugin);
-
+Vue.use(VuejsDialog);
 
 const el = document.getElementById('app')
 
