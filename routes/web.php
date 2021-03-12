9ad0d2b6f6e36d7cdb903f0364302ed83fa6dfaf
Cedric Lima <limacedric@hotmail.fr>
2021-03-12 10:44:02 +0100

    Ajout package pour securiser upload image

--- routes/web.php
+++ routes/web.php
@@ -24,18 +24,18 @@ Route::get('/articles', 'ArticleController@index')->name('article-index');
 Route::get('/articles/categorie/{title}', 'ArticleController@indexCategorie')->name('article-categorie');
 Route::get('/articles/{slug}', 'ArticleController@show')->name('article-show');
 
-Route::post('/commentaire', 'CommentaireController@store')->name('commentaire-store')->middleware('auth');
-Route::patch('/commentaire/edit', 'CommentaireController@edit')->name('commentaire-edit')->middleware('auth');
-Route::delete('/commentaire/delete/{id}', 'CommentaireController@delete')->name('commentaire-delete')->middleware('auth');
-
-Route::post('/like/article', 'LikeController@store')->name('like-post')->middleware('auth');
-Route::post('/dislike/article', 'LikeController@delete')->name('like-delete')->middleware('auth');
-
 Route::get('/photo', 'PhotoController@index')->name('photo-index');
-Route::post('/photo', 'PhotoController@store')->name('photo-store');
+Route::post('/photo', 'PhotoController@store')->name('photo-store')->middleware('image-sanitize');
 Route::delete('/photo/{photo}', 'PhotoController@delete')->name('photo-delete');
 
 Route::group(['middleware' => 'auth'], function () {
+   Route::post('/commentaire', 'CommentaireController@store')->name('commentaire-store');
+   Route::patch('/commentaire/edit', 'CommentaireController@edit')->name('commentaire-edit');
+   Route::delete('/commentaire/delete/{id}', 'CommentaireController@delete')->name('commentaire-delete');
+   
+   Route::post('/like/article', 'LikeController@store')->name('like-post');
+   Route::post('/dislike/article', 'LikeController@delete')->name('like-delete');
+
    Route::get('/account', 'AccountController@index')->name('account-index');
    Route::post('/account/update', 'AccountController@update')->name('account-update');
    Route::delete('/account/delete/{user}', 'AccountController@delete')->name('account-delete');
